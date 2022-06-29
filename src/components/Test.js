365f1c2e42424c9d97119404c203b7eb9d81da2b
AlexBlacksmith <unbloody.alex@gmail.com>
2022-06-29 19:16:14 +0500

    npm packages was added

---
+++ src/components/Test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Test = () => {
+    return (
+        <div className="container text-center">
+            <h2>
+                Hello new Version!
+            </h2>
+        </div>
+    );
+}
+
+export default Test;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.css';
+import Test from "./components/Test";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <div>
+        <Test />
+    </div>    
+);
